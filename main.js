277ff1e6066dc6eab581ff0c87d4d8eeecefe1dc
Ollie <124583003+Olivem1234567@users.noreply.github.com>
2023-11-15 17:35:10 +0000

    weerg

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ $(document).keydown(function(e) {
     if (e.keyCode == 77) {$('#Menu').collapse('show') ;}  ;
     if (e.keyCode == 27) {$('#Menu').collapse('hide') ;}  ;
  }) ;
+ //M=Open ESC=Close//
