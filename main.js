715f770e25a61b44cc085f65e8984dd95f640be3
Ollie <124583003+Olivem1234567@users.noreply.github.com>
2023-11-16 17:24:34 +0000

    hjuwebfhjberg

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+function explodeButton() {
+    var explosion = document.createElement("div");
+    explosion.classList.add("explosion");
+    explosion.style.top = event.clientY + "px";
+    explosion.style.left = event.clientX + "px";
+    document.body.appendChild(explosion);
+ 
+    setTimeout(function() {
+        explosion.remove();
+    }, 500);
+}
